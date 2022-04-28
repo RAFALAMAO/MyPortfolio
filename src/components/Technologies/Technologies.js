51ee073d45356bc268b9380df6e1c81ced0a0507
RAFALAMAO <aaronjt@outlook.com>
2022-04-28 13:11:52 -0500

    Add js icon at teconologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiVisualstudio } from 'react-icons/di';
+import { DiJavascript1, DiReact, DiVisualstudio } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
@@ -31,7 +31,7 @@ const Technologies = () =>  (
       </ListItem>
       <ListItem>
         <picture>
-          <DiFirebase size="3rem" />
+          <DiJavascript1 size="3rem" />
         </picture>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
