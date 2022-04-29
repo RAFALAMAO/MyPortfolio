78442095f698258d4431a4cebc3a2fb7c4c528ec
RAFALAMAO <aaronjt@outlook.com>
2022-04-29 17:35:30 -0500

    Delete proyects animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,13 +24,13 @@ const Home = () => {
           </Section>
         </motion.div>
 
-        <motion.div
+        {/* <motion.div
           initial={{ y: 30, opacity: 0 }}
           animate={{ y: 0, opacity: 1}}
           transition={{ delay: 0.3, duration: 0.6 }}
-        >
+        > */}
            <Projects />
-        </motion.div>
+        {/* </motion.div> */}
 
         <Technologies />
         <Timeline />
